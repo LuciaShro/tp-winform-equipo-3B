b4b36749ee728162fd77bbead7c75d33234ef818
miguelgomez99 <migue8935@gmail.com>
2025-04-14 16:03:23 -0300

    Centralizacion de conexion a BD

--- Gestion/GestionArticulos.cs
+++ Gestion/GestionArticulos.cs
@@ -10,78 +10,41 @@ namespace Gestion
 {
     public class GestionArticulos
     {
-        SqlConnection conexion = new SqlConnection();
-        SqlCommand comando = new SqlCommand();
-        SqlDataReader lector;
-
-        //Agregar articulos
-        /*public void AgregarArticulos (Articulo articulo)
-        {
-        }*/
-
-        //Modificar articulos
-        //public bool ModificarArticulo(int codArticulo)
-        //{
-        //}
-
-        //Eliminar articulos
-        //public bool EliminarArticulo(int codArticulo) 
-        //{ 
-        //}
-
-        //Buscar articulos
-        //public Articulo BuscarArticulo(int codArticulo)
-        //{
-        //}
-
-        //Listar articulo
-        //public void ListarArticulos()
-        //{
-        //}
-
         public List<Articulo> listar()
         {
             List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
 
             try
             {
-                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true;";
-                comando.CommandType = System.Data.CommandType.Text;
-                comando.CommandText = "SELECT Id, Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio from ARTICULOS";
-                comando.Connection = conexion;
+             
+                datos.setearConsulta("SELECT Id, Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio from ARTICULOS");
+                datos.ejecutarLectura();
 
-                conexion.Open();
-                lector = comando.ExecuteReader(); //tabla 
-
-                while (lector.Read())
+                while (datos.Lector.Read())
                 {
                     Articulo aux = new Articulo(); //Lo empezamos a cargar con los datos del lector de ese registro
                     //aux.IDArticulo = lector.GetInt32(0); 
-                    aux.IDArticulo = (int)lector["Id"]; //Lo mismo que la linea de arriba
-                    aux.codArticulo = (string)lector["Codigo"];
-                    aux.Nombre = (string)lector["Nombre"];
-                    aux.Descripcion = (string)lector["Descripcion"];
-                    aux.Marca.Id = (int)lector["IdMarca"];
-                    aux.Categoria.Id = (int)lector["IdCategoria"];
-                    aux.Precio = (decimal)lector["Precio"];
-
-
-
+                    aux.IDArticulo = (int)datos.Lector["Id"]; //Lo mismo que la linea de arriba
+                    aux.codArticulo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    aux.Marca.Id = (int)datos.Lector["IdMarca"];
+                    aux.Categoria.Id = (int)datos.Lector["IdCategoria"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
 
                     lista.Add(aux);
                 }
 
-               
-                conexion.Close();
                 return lista;
             }
             catch (Exception ex)
             {
-                throw;
+                throw ex;
             } 
             finally
             {
-                conexion.Close();
+                datos.cerrarConexion();
             }
          
         }
@@ -89,3 +52,28 @@ namespace Gestion
 
     }
 }
+
+//Agregar articulos
+/*public void AgregarArticulos (Articulo articulo)
+{
+}*/
+
+//Modificar articulos
+//public bool ModificarArticulo(int codArticulo)
+//{
+//}
+
+//Eliminar articulos
+//public bool EliminarArticulo(int codArticulo) 
+//{ 
+//}
+
+//Buscar articulos
+//public Articulo BuscarArticulo(int codArticulo)
+//{
+//}
+
+//Listar articulo
+//public void ListarArticulos()
+//{
+//}
